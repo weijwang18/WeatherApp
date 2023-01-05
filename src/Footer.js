47501eaf07fb50a297685db6a32dc09d675a3f9f
Winnie Wang <weijwang18@gmail.com>
2023-01-05 01:44:02 -0800

    add footer component

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <div className="signature">Coded by Winnie</div>
+      <a href="https://github.com/weijwang18/Weather-app">View on GitHub</a>
+    </div>
+  );
+}
