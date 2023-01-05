ddbb21ffb70a17c4087ea3a9fe080aa43c7aada3
Winnie Wang <weijwang18@gmail.com>
2023-01-05 01:41:29 -0800

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import Header from "./Header.js";
+import Temperature from "./Temperature.js";
+import Footer from "./Footer.js";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="weatherapp">
+          <Header />
+          <Temperature />
+          <Footer />
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
